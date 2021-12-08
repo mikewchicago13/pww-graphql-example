f8c74ab85bb1c0a5a31eb7408dccbde52eb19082
mw <m@mike.us>
2021-12-08 17:06:52 -0600

    got a packaged server to run from inside the build directory

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import * as express from "express";
+import express from "express";
 import {Request, Response} from "express";
 import {graphqlHTTP} from "express-graphql";
 import graphqlOptions from "./graphqlOptions";
