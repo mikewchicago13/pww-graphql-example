54c6d37ec8bedc0468de82dec6fe2c69e96849f2
mw <m@mike.us>
2021-12-08 16:26:52 -0600

    got the simplest possible query and data resolver running
    https://graphql.org/graphql-js/running-an-express-graphql-server/

--- server.ts
+++ server.ts
@@ -1,5 +1,7 @@
 import * as express from "express";
-import { Request, Response } from "express";
+import {Request, Response} from "express";
+import {graphqlHTTP} from "express-graphql";
+import {buildSchema} from "graphql";
 
 const app = express();
 
@@ -8,6 +10,27 @@ const PORT = 4000;
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const rootValue = {
+  hello: () => {
+    console.log("INSIDE_HELLO");
+    return 'Hello world!';
+  },
+};
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue,
+    graphiql: true
+  })
+);
 
 app.listen(PORT, () => {
   console.log("Graphql server now up at port 4000")
