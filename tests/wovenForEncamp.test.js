3df91d91d7c21e600e34f86451e25729f8653cdf
mw <m@mike.us>
2021-12-10 19:48:10 -0600

    created shell so it can be used with woven code example

---
+++ tests/wovenForEncamp.test.js
@@ -0,0 +1,8 @@
+import {wovenForEncamp} from "../wovenForEncamp";
+
+describe('woven for encamp', () => {
+  it('should do something', () => {
+    const actual = wovenForEncamp();
+    expect(actual).toBeTruthy();
+  });
+});
