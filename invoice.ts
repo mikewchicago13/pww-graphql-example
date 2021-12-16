b379694d39781f263a681e377b824696fd691dda
mw <m@mike.us>
2021-12-16 17:03:27 -0600

    created a simple invoice that can get a total

--- invoice.ts
+++ invoice.ts
@@ -1,15 +1,55 @@
-class Money {
-  _dollars: number;
+export class Money {
   _cents: number;
+
+  constructor(dollars: number, cents: number) {
+    this._cents = dollars * 100 + cents;
+  }
+
+  add(money: Money): Money {
+    return new Money(0, this._cents + money._cents);
+  }
 }
 
-class LineItem {
-  _asOf: Date;
-  _amount: Money;
+export class LineItem {
+  static create(
+    asOf: string,
+    dollars: number,
+    cents: number): LineItem{
+    return new LineItem(
+      new Date(Date.parse(asOf)),
+      new Money(dollars, cents));
+  }
+
+  private readonly _asOf: Date;
+  private readonly _amount: Money;
+
+  constructor(asOf: Date, amount: Money) {
+    this._asOf = asOf;
+    this._amount = amount;
+  }
+
+
+  get asOf(): Date {
+    return this._asOf;
+  }
+
+  get amount(): Money {
+    return this._amount;
+  }
 }
 
-class Invoice {
+export class Invoice {
   _lineItems: LineItem[]
-}
 
-export default Invoice;
+  constructor(lineItems: LineItem[]) {
+    this._lineItems = lineItems;
+  }
+
+  get total(): Money{
+    return this._lineItems
+      .map(x => x.amount)
+      .reduce(
+        (a, b) => a.add(b),
+        new Money(0, 0));
+  }
+}
--- tests/invoice.test.ts
+++ tests/invoice.test.ts
@@ -1,10 +1,19 @@
 // wovenForEncamp.test.js
 
-import Invoice from "../invoice";
+import {Invoice, LineItem, Money} from "../invoice";
 
 describe('Invoice', () => {
-  const actual = new Invoice();
+  const actual = new Invoice(
+    [
+      LineItem.create("2021-01-01", 1, 99),
+      LineItem.create("2021-01-02", 2, 2),
+    ]
+  );
   it('should create an instance', () => {
     expect(actual).toBeTruthy();
+  });
+
+  it('should have a total', () => {
+    expect(actual.total).toStrictEqual(new Money(4, 1));
   });
 });
