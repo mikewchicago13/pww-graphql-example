44d0f78a34083581a7acf707c32d5d4e014c4628
mw <m@mike.us>
2021-12-16 20:31:34 -0600

    replaced explicit call to toString() with implicit conversion by adding a string

--- tests/invoice.test.ts
+++ tests/invoice.test.ts
@@ -1,4 +1,5 @@
 import chai from "chai";
+
 const {assert} = chai;
 chai.config.truncateThreshold = 0;
 
@@ -7,8 +8,8 @@ import {Invoice, LineItem, Money} from "../invoice";
 describe('Invoice', () => {
   const actual = new Invoice(
     [
-      LineItem.create("2021-01-01", new Money( 1, 99), 1),
-      LineItem.create("2021-02-02", new Money( 2, 2), 1),
+      LineItem.create("2021-01-01", new Money(1, 99), 1),
+      LineItem.create("2021-02-02", new Money(2, 2), 1),
     ]
   );
   it('should have a total', () => {
@@ -29,8 +30,8 @@ describe('Invoice', () => {
   describe('multiple items', () => {
     const actual = new Invoice(
       [
-        LineItem.create("2021-01-01", new Money( 1, 0), 7),
-        LineItem.create("2021-02-02", new Money( 2, 0), 1),
+        LineItem.create("2021-01-01", new Money(1, 0), 7),
+        LineItem.create("2021-02-02", new Money(2, 0), 1),
       ]
     );
     it('should have a total', () => {
@@ -42,15 +43,15 @@ describe('Invoice', () => {
 describe('Money', () => {
   describe('display', () => {
     it('zero', () => {
-      assert.deepEqual(new Money(0, 0).toString(),
+      assert.deepEqual(new Money(0, 0) + "",
         "0.00");
     });
     it('1000.01', () => {
-      assert.deepEqual(new Money(1_000, 1).toString(),
+      assert.deepEqual(new Money(1_000, 1) + "",
         "1,000.01");
     });
     it('123456789.99', () => {
-      assert.deepEqual(new Money(123_456_789, 99).toString(),
+      assert.deepEqual(new Money(123_456_789, 99) + "",
         "123,456,789.99");
     });
   });
