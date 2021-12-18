4eb2f656d1b9c96ff9457c0640d68e040b59dd71
mw <m@mike.us>
2021-12-18 17:57:42 -0600

    got all gutter ball tests passing

--- graphql/scoreSheet.ts
+++ graphql/scoreSheet.ts
@@ -1,6 +1,19 @@
 class Frame {
+  private readonly _rolls: number[];
+
+  constructor(rolls: number[]) {
+    this._rolls = rolls.filter(value => value !== undefined);
+  }
+
   get ballsThrown(): number[] {
-    return [];
+    return this._rolls;
+  }
+
+  get runningScore(): number | undefined {
+    if (this._rolls.length) {
+      return this._rolls.reduce((a, b) => a + b, 0);
+    }
+    return undefined;
   }
 }
 
@@ -12,6 +25,21 @@ export class ScoreSheet {
   }
 
   get frames(): Frame[] {
-    return new Array(10).fill(new Frame());
+    return new Array(10).fill(0)
+      .map((_, frameIndex) => {
+        const rollIndex = frameIndex * 2;
+
+        return new Frame(frameIndex === 9 ?
+          this.getRollsInTenthFrame() :
+          this.getRollsInFirstNineFrames(rollIndex));
+      });
+  }
+
+  private getRollsInFirstNineFrames(rollIndex: number): number[] {
+    return [this._rolls[rollIndex], this._rolls[rollIndex + 1]];
+  }
+
+  private getRollsInTenthFrame(): number[] {
+    return [this._rolls[18], this._rolls[19], this._rolls[20]];
   }
 }
--- tests/scoreSheet.test.js
+++ tests/scoreSheet.test.js
@@ -8,12 +8,35 @@ describe('scoreSheet', () => {
       expect(actual.frames).toHaveLength(FRAMES);
     });
     for (let i = 0; i < FRAMES; i++) {
-      it('frame ' + i + ' should have zero balls thrown', () => {
+      it('frame ' + (i+1)+ ' should have zero balls thrown', () => {
         expect(actual.frames[i].ballsThrown).toHaveLength(0);
       });
-      it('frame ' + i + ' should have running score of undefined', () => {
+      it('frame ' + (i+1) + ' should have running score of undefined', () => {
         expect(actual.frames[i].runningScore).toBeUndefined();
       });
     }
+  });
+
+  describe('all zeros', () => {
+    let game = bowling();
+    for (let i = 0; i < 21; i++) {
+      game = game.roll(0);
+    }
+    const actual = game.scoreSheet;
+    for (let i = 0; i < 9; i++) {
+      it('frame ' + (i+1) + ' should have zero balls thrown', () => {
+        expect(actual.frames[i].ballsThrown).toStrictEqual([0, 0]);
+      });
+      it('frame ' + (i+1) + ' should have running score of 0', () => {
+        expect(actual.frames[i].runningScore).toBe(0);
+      });
+    }
+
+    it('frame ' + 10 + ' should have zero balls thrown', () => {
+      expect(actual.frames[9].ballsThrown).toStrictEqual([0, 0, 0]);
+    });
+    it('frame ' + 10 + ' should have running score of 0', () => {
+      expect(actual.frames[9].runningScore).toBe(0);
+    });
   });
 });
