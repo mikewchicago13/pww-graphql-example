68d05bd8aaf5b60f728af74dc73132e1b5c427dc
mw <m@mike.us>
2021-12-18 13:50:11 -0600

    use proper equals

--- graphql/bookRepository.ts
+++ graphql/bookRepository.ts
@@ -83,6 +83,6 @@ export class BookRepository {
   booksWrittenBy(authorId: string): Book[] {
     console.log("INSIDE_BOOKS_WRITTEN_BY " + authorId);
     return books
-      .filter(book => authorId == book.authorId)
+      .filter(book => authorId === book.authorId)
   }
 }
