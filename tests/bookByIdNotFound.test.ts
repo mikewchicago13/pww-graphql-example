bc7a9f15b253e87edd2e90d3bd79cf91d3e396e8
mw <m@mike.us>
2021-12-18 13:28:19 -0600

    added a test to prove there are errors when looking for a book that does not exist

---
+++ tests/bookByIdNotFound.test.ts
@@ -0,0 +1,21 @@
+import graphqlClient from "./graphqlClient";
+import { v4 as uuidv4 } from 'uuid';
+
+describe('can connect to local graphql', () => {
+  const madeUpId = uuidv4();
+  const query = /* GraphQL */
+      `{
+          bookById(id: "${madeUpId}"){
+              id
+          }
+      }`;
+  let errors: any;
+  beforeAll(async () => {
+    const json = await graphqlClient({query});
+    errors = json.errors;
+  })
+
+  it('should have a result', async () => {
+    expect(errors).toBeTruthy();
+  });
+});
