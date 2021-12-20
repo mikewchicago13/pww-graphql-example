ec4911aaf5838833ac6ba50c7578e27fbc48452f
mw <m@mike.us>
2021-12-20 17:47:26 -0600

    removed inappropriate multiple assertions per test

--- tests/bowling/bowlingGame.test.ts
+++ tests/bowling/bowlingGame.test.ts
@@ -14,21 +14,29 @@ describe('bowlingGame', () => {
       .roll(1)
       .score).toBe(1);
   });
-  it('should roll 20 straight 1s', () => {
+  describe('should roll 20 straight 1s', () => {
     const game: Game = new Array(20).fill(1)
       .reduce((g: Game) => g.roll(1), bowlingGame())
-    expect(game.score).toBe(20);
+    it('should total 20', () => {
+      expect(game.score).toBe(20);
+    });
     for (let i = 0; i < 10; i++) {
-      expect(game.scoreUpToFrame(i)).toBe((i + 1) * 2);
+      it('should have 2 per frame ' + (i + 1), () => {
+        expect(game.scoreUpToFrame(i)).toBe((i + 1) * 2);
+      });
     }
   });
-  it('should roll 21 straight 5s', () => {
+  describe('should roll 21 straight 5s', () => {
     const game = new Array(21).fill(1)
       .reduce((g: Game) => g.roll(5), bowlingGame());
-    expect(game.score).toBe(150);
+    it('should total 150', () => {
+      expect(game.score).toBe(150);
+    });
 
     for (let i = 0; i < 10; i++) {
-      expect(game.scoreUpToFrame(i)).toBe((i + 1) * 15);
+      it('should have 15 per frame ' + (i + 1), () => {
+        expect(game.scoreUpToFrame(i)).toBe((i + 1) * 15);
+      });
     }
   });
   it('should pick up a spare', () => {
@@ -41,14 +49,16 @@ describe('bowlingGame', () => {
       .reduce((g: Game) => g.roll(10), bowlingGame());
     expect(game.score).toBe(30);
   });
-  it('should roll a perfect game', () => {
-    let game =new Array(12).fill(1)
+  describe('should roll a perfect game', () => {
+    const game = new Array(12).fill(1)
       .reduce((g: Game) => g.roll(10), bowlingGame());
-
-    expect(game.score).toBe(300);
-
+    it('should total 300', () => {
+      expect(game.score).toBe(300);
+    });
     for (let i = 0; i < 10; i++) {
-      expect(game.scoreUpToFrame(i)).toBe((i + 1) * 30);
+      it('should have 30 per frame ' + (i + 1), () => {
+        expect(game.scoreUpToFrame(i)).toBe((i + 1) * 30);
+      });
     }
   });
   it('should roll a 299', () => {
@@ -57,17 +67,25 @@ describe('bowlingGame', () => {
       .roll(9);
     expect(game.score).toBe(299);
   });
-  it('should gutter until 10th frame, then strike out', () => {
-    const game = new Array(18).fill(1)
-      .reduce((g: Game) => g.roll(0), bowlingGame());
-    for (let i = 0; i < 18; i++) {
-      expect(game.scoreUpToFrame(Math.trunc(i / 2))).toBe(0);
+
+  describe('should gutter until 10th frame, then strike out', () => {
+    function eighteenGutters(): Game {
+      return new Array(18).fill(1)
+        .reduce((g: Game) => g.roll(0), bowlingGame());
     }
-    expect(game
-      .roll(10)
-      .roll(10)
-      .roll(10)
-      .score).toBe(30);
+
+    for (let i = 0; i < 9; i++) {
+      it('should have zeros in frame ' + (i + 1), () => {
+        expect(eighteenGutters().scoreUpToFrame(i)).toBe(0);
+      });
+    }
+    it('should total 30', () => {
+      expect(eighteenGutters()
+        .roll(10)
+        .roll(10)
+        .roll(10)
+        .score).toBe(30);
+    });
   });
   it('should gutter, spare, strike, one to prove that only first ball 10s are counted as strike', () => {
     const game = bowlingGame()
