52bd14e82db05051dc9a50aaf20c34ebdf7b358e
mw <m@mike.us>
2021-12-20 15:17:26 -0600

    removed some other exercises which were here just to avoid the overhead of setting up a new project

--- getProductsOfAllIntsExceptAtIndex.ts
+++
@@ -1,9 +0,0 @@
-export function getProductsOfAllIntsExceptAtIndex(numbers: number[]): number[] {
-  const numbersReplacedWithOneForMultiplication: number[] = new Array(numbers.length);
-
-  for (let i = 0; i < numbers.length; i++) {
-    numbersReplacedWithOneForMultiplication[i] = numbers.slice(0, i).concat(numbers.slice(i + 1))
-      .reduce((a, b) => a * b, 1);
-  }
-  return numbersReplacedWithOneForMultiplication;
-}
