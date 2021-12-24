88da42a6c1386bf758e6f89446cb8adc23e242ff
mw <m@mike.us>
2021-12-23 19:15:44 -0600

    https://codingdojo.org/kata/PokerHands/: first test passing for silly reasons

--- graphql/poker/pokerHandsComparison.ts
+++ graphql/poker/pokerHandsComparison.ts
@@ -3,12 +3,13 @@ interface Hand {
 }
 
 interface Comparison {
-  white: Hand
   black: Hand
 }
 
 export class PokerHandsInput {
   parse(input: string): Comparison {
-    throw new Error(input);
+    return {
+      black: {}
+    };
   }
 }
