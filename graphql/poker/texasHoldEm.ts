b9bec14e33c240274eb87eacef98b2454d3f90d3
mw <m@mike.us>
2021-12-24 18:26:17 -0600

    https://codingdojo.org/kata/TexasHoldEm/: Got the winner identified properly, but did not preserve the order

--- graphql/poker/texasHoldEm.ts
+++ graphql/poker/texasHoldEm.ts
@@ -4,8 +4,10 @@ import {Card, Hand} from "./pokerHandsComparison";
 export class TexasHoldEmHand {
   private readonly _cards: Card[];
   private readonly _allPossibleHands: Hand[];
+  private readonly _name: string;
 
   constructor(cards: string) {
+    this._name = cards;
     this._cards = cards.split(" ").map(x => new Card(x));
     this._allPossibleHands = TexasHoldEmHand._combinations(
       this._cards,
@@ -28,18 +30,71 @@ export class TexasHoldEmHand {
       .flat();
   }
 
-  get cards(): Card[] {
-    return this._cards;
+  get name(): string{
+    return this._name;
   }
 
   get allPossibleHands(): Hand[] {
     return this._allPossibleHands;
+  }
+
+  get description(): string {
+    return this.hasEnoughCards() ? this._bestHand().description : "";
+  }
+
+  public hasEnoughCards() {
+    return this._cards.length === 7;
+  }
+
+  private _bestHand(): Hand {
+    const bestToWorst = (a: Hand, b: Hand) => {
+      if (a > b) {
+        return -1;
+      }
+      if (b > a) {
+        return 1;
+      }
+      return 0;
+    };
+    return this._allPossibleHands.sort(bestToWorst)[0];
+  }
+
+  toString(): string {
+    return this.hasEnoughCards() ? this._bestHand() + "" : "";
   }
 }
 
 export class TexasHoldEm {
   static parse(input: string): TexasHoldEmHand[] {
     return input.split(EOL)
+      .map(x => x.trim())
       .map(value => new TexasHoldEmHand(value));
+  }
+
+  static toOutput(input: string): string {
+    const texasHoldEmHands = TexasHoldEm.parse(input);
+    const bestToWorst = (a: TexasHoldEmHand, b: TexasHoldEmHand) => {
+      if(!b.hasEnoughCards()){
+        return -1;
+      }
+      if(!a.hasEnoughCards()){
+        return 1;
+      }
+      if (a > b) {
+        return -1;
+      }
+      if (b > a) {
+        return 1;
+      }
+      return 0;
+    };
+
+    const winner = texasHoldEmHands.sort(bestToWorst)[0];
+
+    return texasHoldEmHands
+      .map((value) => {
+        return `${value.name} ${value.description} ${value === winner ? "(winner)": ""}`.trim()
+      })
+      .join(EOL);
   }
 }
--- tests/poker/texasHoldEm.test.ts
+++ tests/poker/texasHoldEm.test.ts
@@ -1,4 +1,5 @@
 import {TexasHoldEm, TexasHoldEmHand} from "../../graphql/poker/texasHoldEm";
+import {EOL} from "os";
 
 describe("texas hold 'em", () => {
   const input = `KC 9S KS KD 9D 3C 6D
@@ -7,19 +8,28 @@ describe("texas hold 'em", () => {
   9H 5S
   4D 2D KS KD 9D 3C 6D
   7S TS KS KD 9D`;
-  const expected = `KC 9S KS KD 9D 3C 6D Full House (winner)
-  9C AH KS KD 9D 3C 6D Two Pairs
-  AC QC KS KD 9D 3C 
-  9H 5S 
-  4D 2D KS KD 9D 3C 6D Flush
-  7S TS KS KD 9D`
+
+  const expected = `KC 9S KS KD 9D 3C 6D Full House: Ks over 9s (winner)
+9C AH KS KD 9D 3C 6D Two Pairs: Ks and 9s
+AC QC KS KD 9D 3C 
+9H 5S 
+4D 2D KS KD 9D 3C 6D Flush: K high in Diamonds
+7S TS KS KD 9D`
 
   it('should split by line', () => {
     expect(TexasHoldEm.parse(input)).toHaveLength(6);
   });
+  const actual = TexasHoldEm.toOutput(input).split(EOL);
+  expected.split(EOL)
+    .map(x => x.trim())
+    .forEach((value, index) => {
+      it(`${index} should match output`, () => {
+        expect(actual[index]).toBe(value);
+      });
+    })
 
   describe('create all possible hands', () => {
-    const cards = "1D 2D 3D 4D 5D 6D 7D";
+    const cards = "AD 2D 3D 4D 5D 6D 7D";
     it('should have 7 choose 5 = 21 possible hands', () => {
       expect(new TexasHoldEmHand(cards).allPossibleHands).toHaveLength(21);
     });
@@ -32,9 +42,16 @@ describe("texas hold 'em", () => {
       expect(uniqueHands).toHaveLength(21);
     });
     new TexasHoldEmHand(cards).allPossibleHands.forEach(value => {
-      it(`${value.rawCards} ${value.name}`, () => {
+      it(`${value.rawCards} ${value.description}`, () => {
         expect(value.rawCards.length).toBe(5);
       });
+      it(`${value}`, () => {
+        expect(value).toBeTruthy();
+      });
     })
+
+    it('should choose the best hand', () => {
+      expect(new TexasHoldEmHand(cards).description).toMatch(/Straight Flush: 7 high in Diamonds/);
+    });
   });
 });
