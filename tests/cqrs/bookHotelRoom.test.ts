f44bcb7a2c593c941a7fc0c4168f98f244d3d408
mw <m@mike.us>
2021-12-25 17:21:18 -0600

    https://codingdojo.org/kata/CQRS_Booking/: got more tests present for different reservation scenarios

--- tests/cqrs/bookHotelRoom.test.ts
+++ tests/cqrs/bookHotelRoom.test.ts
@@ -14,7 +14,7 @@ describe('book hotel room', () => {
 
     it('should not have booked room available', () => {
       new CommandService().bookARoom({
-        arrivalDate: new Date("2021-12-25"),
+        arrivalDate: new Date("2021-12-24"),
         clientId: "test",
         departureDate: new Date("2021-12-26"),
         roomName: bookedRoom
@@ -23,10 +23,28 @@ describe('book hotel room', () => {
         .freeRooms(new Date("2021-12-25"), new Date("2021-12-26"))
         .map(x => x.roomName))
         .not.toContain(bookedRoom);
+      expect(queryService
+        .freeRooms(new Date("2021-12-24"), new Date("2021-12-25"))
+        .map(x => x.roomName))
+        .not.toContain(bookedRoom);
+      expect(queryService
+        .freeRooms(new Date("2021-12-24"), new Date("2021-12-26"))
+        .map(x => x.roomName))
+        .not.toContain(bookedRoom);
+      expect(queryService
+        .freeRooms(new Date("2021-12-01"), new Date("2021-12-31"))
+        .map(x => x.roomName))
+        .not.toContain(bookedRoom);
     });
-    it('should be available for booking on a different day', () => {
+    it('should be available for booking after booking expires', () => {
       expect(queryService
         .freeRooms(new Date("2021-12-26"), new Date("2021-12-27"))
+        .map(x => x.roomName))
+        .toContain(bookedRoom);
+    });
+    it('should be available for booking before booking starts', () => {
+      expect(queryService
+        .freeRooms(new Date("2021-12-21"), new Date("2021-12-24"))
         .map(x => x.roomName))
         .toContain(bookedRoom);
     });
