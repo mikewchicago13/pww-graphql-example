8bd0362b8c31fdde3c095a22910fb345a4445194
mw <m@mike.us>
2021-12-26 17:35:20 -0600

    https://www.codewars.com/kata/539a0e4d85e3425cb0000a88/train/typescript: only use one parameter at a time

--- graphql/codewars/chainAdd.ts
+++ graphql/codewars/chainAdd.ts
@@ -1,7 +1,6 @@
 export default function add(x: number): any {
-  const func = function (): any {
-    const args = Array.prototype.slice.call(arguments);
-    return add(args.reduce((a: number, b: number) => a + b, x))
+  const func = function (y: number): any {
+    return add(x + y)
   };
   func.valueOf = () => x
   return func
