c91941d9165048c07bec6134b481ce47321141d4
mw <m@mike.us>
2021-12-27 17:10:27 -0600

    secured all the mutation methods in a loop, instead of relying on each one to be secured

--- graphql/mutation.ts
+++ graphql/mutation.ts
@@ -1,6 +1,15 @@
 import {secure} from "./secure";
 import {BookRepository} from "./books/bookRepository";
 
-export default {
-  addBook: secure(BookRepository.addBook),
+const mutations: any = {
+  addBook: BookRepository.addBook,
 };
+
+function secureAllMethods() {
+  Object.keys(mutations)
+    .forEach(key => mutations[key] = secure(mutations[key]));
+}
+
+secureAllMethods();
+
+export default mutations;
