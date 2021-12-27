3599a8717b5164ee0fe5073f52f7b5b44db141a3
mw <m@mike.us>
2021-12-27 16:08:15 -0600

    using built in data structures instead of using {} as a map, part 5

--- graphql/cqrs/dateUtilities.ts
+++ graphql/cqrs/dateUtilities.ts
@@ -1,14 +1,16 @@
+import {DatePart} from "./types";
+
 export class DateUtilities {
   static nextDay(date: Date): Date {
     return new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1,
       date.getHours(), date.getMinutes(), date.getSeconds(), date.getMilliseconds());
   }
 
-  static datePart(date: Date): string {
+  static datePart(date: Date): DatePart {
     return date.toISOString().split("T")[0];
   }
 
-  static isDatePartBetween(reservationDate: string, arrivalDate: string, departureDate: string): boolean {
+  static isDatePartBetween(reservationDate: DatePart, arrivalDate: DatePart, departureDate: DatePart): boolean {
     return arrivalDate <= reservationDate && reservationDate < departureDate;
   }
 }
