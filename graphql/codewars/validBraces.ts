070c543a9d5e504f61bcc73d517c65860e4bff42
mw <m@mike.us>
2021-12-27 16:32:47 -0600

    use set instead of {} as map and checking keys

--- graphql/codewars/validBraces.ts
+++ graphql/codewars/validBraces.ts
@@ -1,21 +1,22 @@
 export function validBraces(braces: string): boolean {
   const stack: string[] = [];
 
-  const openers: Set<String> = new Set<String>(["{", "(", "["]);
+  const openers: Set<string> = new Set<string>(["{", "(", "["]);
 
-  const closers: any = {
-    ")": "(",
-    "}": "{",
-    "]": "["
-  }
+  const closers: Map<string, string> = new Map<string, string>([
+    [")", "("],
+    ["}", "{"],
+    ["]", "["]
+  ]);
+
   for (let i = 0; i < braces.length; i++) {
     const char = braces[i];
     if (openers.has(char)) {
       stack.push(char);
     }
-    if (char in closers) {
+    if (closers.has(char)) {
       const pop = stack.pop();
-      if (closers[char] !== pop) {
+      if (closers.get(char) !== pop) {
         return false;
       }
     }
