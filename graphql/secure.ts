0dedc0268e0cf55c6ff878e8cf860e088b68120d
mw <m@mike.us>
2022-01-07 16:30:47 -0600

    seeing what a sample pull request looks like

--- graphql/secure.ts
+++ graphql/secure.ts
@@ -3,7 +3,7 @@ import {Request} from "express";
 export function secure(func: Function) {
   return (context: unknown, args: unknown, req: Request) => {
     if (req.headers.authorization === 'secret') {
-      return func(context, args);
+      return func(context, args, req);
     }
     if (req && req.res) {
       req.res.status(401);
